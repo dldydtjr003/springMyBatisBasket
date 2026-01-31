5621220f28c6c81d44d5b12ec25cb293e2499838
dldydtjr003 <dldydtjr002@gmail.com>
2026-01-31 23:59:11 +0900

    [추가] userServiceImpl 생성

--- springMyBatisBasket/src/main/java/com/zeus/service/UserServiceImpl.java
+++ springMyBatisBasket/src/main/java/com/zeus/service/UserServiceImpl.java
@@ -2,38 +2,50 @@ package com.zeus.service;
 
 import java.util.List;
 
-import com.zeus.domain.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.zeus.domain.Users;
+import com.zeus.mapper.UserMapper;
+
+@Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private UserMapper userMapper;
 	@Override
+	@Transactional
 	public int create(Users user) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = userMapper.create(user);
+		return count;
+		
 	}
 
 	@Override
 	public List<Users> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Users> userList = userMapper.list();
+		return userList;
 	}
 
 	@Override
-	public Users read(Users user) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Users read(Users u) throws Exception {
+		Users user = userMapper.read(u);
+		return user;
 	}
 
 	@Override
+	@Transactional
 	public int update(Users user) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = userMapper.update(user);
+		return count;
 	}
 
 	@Override
+	@Transactional
 	public int delete(Users user) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = userMapper.delete(user);
+		return count;
 	}
 
 }
